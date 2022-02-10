e21920f3c10f3802d14a254d28dd1473e838d196
Logan Krashnak <lak801@yahoo.com>
2022-02-10 18:46:48 -0500

    added ssl certificate

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,13 @@ function Footer() {
   return (
     <footer className="footer fixed-bottom top-and-bottom">
       <p className="text-center text-white">The Roofer's of MD, LLC</p>
+      <span id="siteseal">
+        <script
+          async
+          type="text/javascript"
+          src="https://seal.godaddy.com/getSeal?sealID=3VWi26t6M7otnkgT1m0cen1Mq9B6BuxTP2rDlDPnJY7CPH2EHCqfXRj4udtQ"
+        ></script>
+      </span>
     </footer>
   );
 }
