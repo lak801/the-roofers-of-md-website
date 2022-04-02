add7f36f3735bd08978ecfb17fd3ddae9caf0a7b
Logan Krashnak <lak801@yahoo.com>
2022-04-02 16:51:32 -0400

    added new number!

--- src/components/Call.jsx
+++ src/components/Call.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
-import { AiOutlineMail, AiOutlinePhone } from "react-icons/ai";
+import { AiOutlinePhone } from "react-icons/ai";
+import { HiOutlineMail } from "react-icons/hi";
 function Call() {
   return (
     <Container id="float">
-      {/* <Container id="phone">
-        <AiOutlinePhone /> 443-739-7882{" "}
-      </Container> */}
+      <Container id="phone">
+        <AiOutlinePhone />
+        <p>443-569-1511</p>
+      </Container>
 
       <Container id="email">
-        <AiOutlineMail />
-        <p>info@theroofersofmd.com</p>
+        <HiOutlineMail />
+        <p id="email-text">info@theroofersofmd.com</p>
       </Container>
     </Container>
   );
